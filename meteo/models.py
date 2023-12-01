1aa4ee47b0ff4f769063d538aa1aac6a840f23c8
ivorav <ivorav@amazon.com>
2023-12-01 16:38:43 +0100

    First upload

---
+++ meteo/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Worldcities(models.Model):
+    city = models.TextField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    lng = models.FloatField(blank=True, null=True)
+    country = models.TextField(blank=True, null=True)
+    id = models.IntegerField(blank=True, primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'worldcities'
